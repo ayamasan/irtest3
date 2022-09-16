273a50c616094b7ddf6df88b22a539de9f27b5ac
ayamasan <ayamap@nifty.com>
2022-09-16 16:44:37 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,15 @@ let カウンター = 0
 basic.showIcon(IconNames.Square)
 let 値 = 0
 pins.digitalWritePin(DigitalPin.P14, 0)
+/**
+ * IR送信テスト
+ * 
+ * 搬送波
+ * 
+ * ループ回数10＝25us、40KHz
+ * 
+ * ループ回数12＝28us、36KHz
+ */
 basic.forever(function () {
     for (let index = 0; index < 10; index++) {
         if (値 == 0) {
