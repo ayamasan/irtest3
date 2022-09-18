7d818e6694b331194f25a083e407fd8718a9d3cc
ayamasan <ayamap@nifty.com>
2022-09-18 16:01:04 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,25 @@
 input.onButtonPressed(Button.B, function () {
-	
+    for (let index = 0; index < ヘッダー1長; index++) {
+        pins.digitalWritePin(DigitalPin.P14, 1)
+        for (let index = 0; index < 3; index++) {
+            カウンター = pins.digitalReadPin(DigitalPin.P0)
+        }
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        for (let index = 0; index < 周波数長; index++) {
+            カウンター = pins.digitalReadPin(DigitalPin.P0)
+        }
+    }
+    for (let index = 0; index < ヘッダー2長; index++) {
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        for (let index = 0; index < 3; index++) {
+            カウンター = pins.digitalReadPin(DigitalPin.P0)
+        }
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        for (let index = 0; index < 周波数長; index++) {
+            カウンター = pins.digitalReadPin(DigitalPin.P0)
+        }
+    }
+    basic.pause(100)
 })
 /**
  * IR送信テスト
@@ -11,6 +31,9 @@ input.onButtonPressed(Button.B, function () {
  * ループ回数12＝28us、36KHz
  */
 let カウンター = 0
+let ヘッダー2長 = 0
+let ヘッダー1長 = 0
+let 周波数長 = 0
 basic.showIcon(IconNames.Square)
 let 値 = 0
 pins.digitalWritePin(DigitalPin.P14, 0)
@@ -22,21 +45,8 @@ let IRデータ = [
 "8",
 "0"
 ]
-let 周波数長 = 6
-let ヘッダー1長 = 0
-let ヘッダー2長 = 0
-let _1T = 0
-let _3T = 0
-basic.forever(function () {
-    for (let index = 0; index < 10; index++) {
-        pins.digitalWritePin(DigitalPin.P14, 1)
-        for (let index = 0; index < 3; index++) {
-            カウンター = pins.digitalReadPin(DigitalPin.P0)
-        }
-        pins.digitalWritePin(DigitalPin.P14, 0)
-        for (let index = 0; index < 周波数長; index++) {
-            カウンター = pins.digitalReadPin(DigitalPin.P0)
-        }
-    }
-    basic.pause(100)
-})
+周波数長 = 6
+ヘッダー1長 = 96
+ヘッダー2長 = 24
+let _1T = 24
+let _3T = 72
