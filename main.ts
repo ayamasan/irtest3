470f815aeac173e414be2415e81cd3d83c95d80d
ayamasan <ayamap@nifty.com>
2022-09-18 15:39:39 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,14 +13,12 @@ pins.digitalWritePin(DigitalPin.P14, 0)
  */
 basic.forever(function () {
     for (let index = 0; index < 10; index++) {
-        if (値 == 0) {
-            値 = 1
-            pins.digitalWritePin(DigitalPin.P14, 1)
-        } else {
-            値 = 0
-            pins.digitalWritePin(DigitalPin.P14, 0)
+        pins.digitalWritePin(DigitalPin.P14, 1)
+        for (let index = 0; index < 3; index++) {
+            カウンター = pins.digitalReadPin(DigitalPin.P0)
         }
-        for (let index = 0; index < 12; index++) {
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        for (let index = 0; index < 7; index++) {
             カウンター = pins.digitalReadPin(DigitalPin.P0)
         }
     }
